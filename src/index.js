f4464a31e50c60fad9fa5bb7e20992da31128f47
thanhnx9368 <rainlove9876@gmail.com>
2021-08-11 01:16:03 +0700

    setup template engine, nodemon, morgan

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const port = 3000
+
+const app = express()
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => console.log(` http://localhost:${port}`))
