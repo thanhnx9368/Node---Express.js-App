fa6fda57dedfb4f024afcbd5600670dc5249e49e
thanhnx9368 <rainlove9876@gmail.com>
2021-08-15 18:54:03 +0700

    add routes for create course

--- src/routes/course.js
+++ src/routes/course.js
@@ -8,6 +8,7 @@ router.delete('/:id', CourseController.delete)
 router.delete('/:id/force', CourseController.forceDestroy)
 router.get('/:id/edit', CourseController.edit)
 router.put('/:id', CourseController.update)
+router.get('/create', CourseController.create)
 router.get('/:slug', CourseController.show)
 
 module.exports = router
